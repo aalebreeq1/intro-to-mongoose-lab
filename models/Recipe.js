3564e9298615f90baf83d1b62145131cf83e5467
aalebreeq1 <abdullaebreeq1@gmail.com>
2026-09-02 19:36:54 +0300

    complete(method 4): create an getAllRecipes()

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const dns = require('dns')
+dns.setServers(['8.8.8.8', '1.1.1.1'])
 const recipeSchema = new mongoose.Schema({
     name: {
         type: String,
--- practiceDatabaseMethods.js
+++ practiceDatabaseMethods.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Recipe = require('./models/Recipe')
-const dns= require('dns')
+const dns = require('dns')
 dns.setServers(['8.8.8.8', '1.1.1.1'])
 const dotenv = require('dotenv').config()
 
@@ -18,6 +18,44 @@ async function connectToDB() { //connection to the database
 
 connectToDB() // connect to database
 
+const newRecipe = {
+    name: "Um Ali",
+    instructions: "bake at 180C",
+    prepTime: 120,
+    difficulty: "Medium"
+}
+
+
+
+async function createRecipe(newRecipe) {
+    try {
+        const createRecipe = await Recipe.create(newRecipe)
+        console.log(createRecipe)
+    }
+    catch (err) {
+        console.log(err)
+    }
+
+}
+
+// createRecipe(newRecipe)
+
+async function getAllRecipies() {
+    try {
+        const recipies = await Recipe.find()
+
+        for (let recipie of recipies) {
+            console.log(
+                `${recipie.name} is an ${recipie.difficulty} recipe and takes ${recipie.prepTime} minutes to prepare`
+            )
+        }
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+getAllRecipies()
 
 
 
